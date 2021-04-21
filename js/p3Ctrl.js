9da75968e9d0d36881dc8a7b189ad92a56aa2207
Vincent <vincent.durand@maincare.fr>
2021-04-21 14:23:48 +0200

    bugged

--- js/p3Ctrl.js
+++ js/p3Ctrl.js
@@ -1,5 +1,6 @@
 // Controleur de la page p3
 // $location est en lien avec le système de routage (redirection par ex)
-app.controller("p3Ctrl", function($scope, $location) {
-    $scope.prenom = ""
+app.controller("p3Ctrl", function($scope, $routeParams) {
+    $scope.prenom = $routeParams.prenom;
+    console.log($routeParams);
 });
